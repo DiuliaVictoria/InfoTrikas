57fea90f8101347876bc86a27821bcd3a80513d9
Diulia Victória <diulia.santos@sptech.school>
2022-06-08 01:31:58 -0300

    versão finalizada do info trikas

--- site/src/controllers/medidaController.js
+++ site/src/controllers/medidaController.js
@@ -1,34 +1,45 @@
 var medidaModel = require("../models/medidaModel");
 
 function buscarQtdAcertos(req, res) {
-
-    medidaModel.buscarQtdAcertos().then((resultado) => {
-        if (resultado.length > 0) {
-            res.status(200).json(resultado);
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
+  medidaModel
+    .buscarQtdAcertos()
+    .then((resultado) => {
+      if (resultado.length > 0) {
+        res.status(200).json(resultado);
+      } else {
+        res.status(204).send("Nenhum resultado encontrado!");
+      }
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log(
+        "Houve um erro ao buscar as ultimas medidas.",
+        erro.sqlMessage
+      );
+      res.status(500).json(erro.sqlMessage);
     });
 }
 
 function buscardadosusuarios(req, res) {
-    let user = [];
-
-    medidaModel.buscarUser().then((response) => {
-        const tamanho = response.length;
-        if (tamanho > 0) {
-            user.push(response)
-
-        }})
-    }
+  medidaModel
+    .buscarUser()
+    .then((response) => {
+      const tamanho = response.length;
+      if (tamanho > 0) {
+        res.json({ response });
+      }
+    })
+    .catch(function (erro) {
+      console.log(erro);
+      console.log(
+        "Houve um erro ao buscar as ultimas medidas.",
+        erro.sqlMessage
+      );
+      res.status(500).json(erro.sqlMessage);
+    });
+}
 
 module.exports = {
-    buscarQtdAcertos,
-    buscardadosusuarios
-    
-
-}
+  buscarQtdAcertos,
+  buscardadosusuarios,
+};
